53850a22838d2521f625224e04817b84cf6368f6
rebelshsjjs <yuhgfgjjjggjjj@gmail.com>
2025-02-11 18:12:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -51,7 +51,7 @@ If You Want To Remove Api Then Copy This And Send To Bot - `/api None`"""
 
 📚 ʟɪʙʀᴀʀʏ: <a href=https://docs.pyrogram.org>𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦</a>
 
-🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/Kingvj01>𝐓𝐞𝐜𝐡 𝐕𝐉</a>
+🧑🏻‍💻 ᴅᴇᴠᴇʟᴏᴘᴇʀ: <a href=https://t.me/Stylish_Star2>RebelStar</a>
 
 👥 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ: <a href=https://t.me/Rebel_Backup>Rebel 𝐒𝐮𝐩𝐩𝐨𝐫𝐭</a>
 
